44999d20d419c140c11fb147556f595a9c1bd36f
Edgar Montenegro <edgarmontenegro321@gmail.com>
2023-10-24 02:45:58 -0300

    🗑️ Delete dead code

--- JS/moneyConversor.js
+++ JS/moneyConversor.js
@@ -1,13 +1,11 @@
 // Create money conversor
 function result() {
     let name = document.getElementById("name").value;
-
     let price = document.getElementById("price").value;
-
     let quantity = document.getElementById("quantity").value;
-
     let res = price * quantity;
 
+    // Reemplazo alert() por una librería llamada sweet alert 2.
     Swal.fire({
         text: `Hi ${name}, the result is: $${res}`,
         icon: "success"
