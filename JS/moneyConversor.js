a8a06f4d6c4b32def6ac06229f09155b103743bb
Edgar Montenegro <edgarmontenegro321@gmail.com>
2023-10-24 02:20:13 -0300

    ➕ Add mini project Money Conversor

---
+++ JS/moneyConversor.js
@@ -0,0 +1,16 @@
+// Create money conversor
+function result() {
+    let name = document.getElementById("name").value;
+
+    let price = document.getElementById("price").value;
+
+    let quantity = document.getElementById("quantity").value;
+
+    let res = price * quantity;
+
+    Swal.fire({
+        title: "Esta es una alerta",
+        text: `Hi ${name}, the result is: ${res}`,
+        icon: "info"
+    });
+}
