ed70d48cf1fe7295eca01e1a900707a0862ac6ff
Edgar Montenegro <edgarmontenegro321@gmail.com>
2023-10-24 03:25:53 -0300

    ➕ Add validation to empty fields

--- JS/moneyConversor.js
+++ JS/moneyConversor.js
@@ -1,13 +1,21 @@
 // Create money conversor
 function result() {
-    let name = document.getElementById("name").value;
-    let price = document.getElementById("price").value;
-    let quantity = document.getElementById("quantity").value;
+    let name = document.getElementById('name').value;
+    let price = document.getElementById('price').value;
+    let quantity = document.getElementById('quantity').value;
     let res = price * quantity;
-
+    
     // Reemplazo alert() por una librería llamada sweet alert 2.
-    Swal.fire({
-        text: `Hi ${name}, the result is: $${res}`,
-        icon: "success"
-    });
+    if(!name || !price || !quantity) {
+        Swal.fire({
+            text: `Hey! We need you to complete all the fields!`,
+            icon: 'warning'
+        })
+    }
+    else{
+        Swal.fire({
+            text: `Hi ${name}, the result is: $${res}`,
+            icon: 'success'
+        })
+    }
 }
